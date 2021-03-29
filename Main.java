aaae699dda4283f26139164c45ce8f1fe495ed11
Alrappie <73277795+alrappie@users.noreply.github.com>
2021-03-29 19:04:49 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -36,74 +36,71 @@ class masuk{
 			tunggu.enqueue(this.nama,this.usia,this.tensi);
 			System.out.println(tungguPasien());
 		}
-	}		
-	public void status() {
-		// CONTOH RETURN
-			//DAFTAR_ANTRE Ana^BUKAN_LANSIA^110 Caca^BUKAN_LANSIA^140
-			// DAFTAR_TUNGGU Dodo^BUKAN_LANSIA^120	
-		// KASUS KOSONG
-			// Jika Queue (Ruang Antre atau Daftar Tunggu) kosong, cukup
-			// cetak "-" . Contoh:
-			// DAFTAR_ ANTRE <spasi> -
-			// DAFTAR_TUNGGU <spasi> -
-			String daftarAntre = "DAFTAR_ANTRE ";
-			if(masuk.antri.isEmpty()){
-				daftarAntre+="-";
-			}
-			else{
-				for(int i = 0; i<antri.index;i++){
-					daftarAntre+=antri.learn.getNamaByIndex(i)+"^"+(antri.usia[i]>=60?"LANSIA":"BUKAN_LANSIA")+"^"+antri.tensi[i]+" ";
-				}
-			}
-			String daftarTunggu = "DAFTAR_TUNGGU ";
-			
-			if(masuk.tunggu.isEmpty()){
-				daftarTunggu+="-";
-			}
-			else{
-				for(int i =0;i<masuk.tunggu.itemCount;i++){
-					daftarTunggu+=tunggu.learn[i]+"^"+(tunggu.umur[i]>=60?"LANSIA":"BUKAN_LANSIA")+"^"+tunggu.tensi[i]+" ";
-				}
-			}
-			System.out.println(daftarAntre+"\n"+daftarTunggu) ;
-	
-	
-		}
-		public void selesai(int N) throws Exception {
+	}
 
-		//init buat ngapus array
-// 			manggil QueueArray
-// 			trus diapus ampe < N
-// 			panggil methodnya..
-// 			...
-		//"SELESAI_VAKSIN+" "+this.name+" "+...
+	public void selesai(int N) throws Exception {
 		if (!antri.isEmpty()) {
 			for (int i = 0; i < N; i++) {
-				System.out.print(" "+antri.learn.head.data);
-				antri.dequeue();
+				if (antri.index!=0){
+					System.out.print(" "+antri.learn.head.data);
+					antri.dequeue();
+				}else {
+					System.out.print(" "+ tunggu.dequeue());
+				}
 			}
-			while (antri.checkBangkuKosong()){
+			System.out.println();
+			while (antri.checkBangkuKosong()&&tunggu.itemCount!=0){
 				antri.bangkuKosong();
 			}
-		}else{
-			System.out.println("ANTRE KOSONG");
 		}
-		System.out.println();
 	}
 	//buat STRING_NAMA
-	public void selesai(String  x) {
-		System.out.println("SELESAI_NAMA");
+	public void selesai(String  x) throws Exception {
 		if (!antri.isEmpty()) {
 // 			manggil QueueArray
 // 			ampe namanya sama
 			antri.selesaiByNama(x);
-			antri.checkBangkuKosong();
-			System.out.print(" "+x);//print klo dah keluar
+			if (antri.checkBangkuKosong()&&tunggu.itemCount!=0){
+				antri.bangkuKosong();
+			}
+			System.out.println("SELESAI_VAKSIN"+" "+x);//print klo dah keluar
 		}else{
 			System.out.println("ANTRE KOSONG");
 		}
 	}
-	
+	public void status() {
+		// CONTOH RETURN
+		//DAFTAR_ANTRE Ana^BUKAN_LANSIA^110 Caca^BUKAN_LANSIA^140
+		// DAFTAR_TUNGGU Dodo^BUKAN_LANSIA^120
+		// KASUS KOSONG
+		// Jika Queue (Ruang Antre atau Daftar Tunggu) kosong, cukup
+		// cetak "-" . Contoh:
+		// DAFTAR_ ANTRE <spasi> -
+		// DAFTAR_TUNGGU <spasi> -
+		String daftarAntre = "DAFTAR_ANTRE ";
+		if(masuk.antri.isEmpty()){
+			daftarAntre+="-";
+		}
+		else{
+			for(int i = 0; i<antri.index;i++){
+				daftarAntre+=antri.learn.getNamaByIndex(i)+"^"+(antri.usia[i]>=60?"LANSIA":"BUKAN_LANSIA")+"^"+antri.tensi[i]+" ";
+			}
+		}
+		String daftarTunggu = "DAFTAR_TUNGGU ";
+
+		if(masuk.tunggu.isEmpty()){
+			daftarTunggu+="-";
+		}
+		else{
+			for(int i =0;i<masuk.tunggu.itemCount;i++){
+				daftarTunggu+=tunggu.learn[i]+"^"+(tunggu.umur[i]>=60?"LANSIA":"BUKAN_LANSIA")+"^"+tunggu.tensi[i]+" ";
+			}
+		}
+		System.out.println(daftarAntre+"\n"+daftarTunggu) ;
+
+
+	}
+
 	public void ukuran(int ukuran) {
 		System.out.println("SUKSES UBAH " + antri.maxLength + " " + ukuran);
 		if (antri.maxLength<ukuran){
@@ -114,8 +111,9 @@ class masuk{
 			antri.arrayDecreasing(antri.maxLength);
 		}
 	}
-	public void skip(String nama) {
-
+	public void skip(String nama) throws Exception {
+		antri.skip(nama);
+		System.out.println("SKIP SUKSES");
 	}
 // 	check kondisi orang yang masuk
 // 	pertama boleh vaksn atau engga
@@ -150,49 +148,51 @@ class masuk{
 		return "TUNGGU"+" "+this.nama;
 	}
 }
-
+//"/C:\\Users\\alrav\\Dev\\Kuliah\\Semester-2\\Algorithhm Structure Data\\Practicise\\Tugas01\\tes.txt"
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		masuk learn=new masuk();
-        FileReader fr = new FileReader("../ASD/src/tugas_1/tes.txt");
-        Scanner inFile = new Scanner(fr);
-        while (inFile.hasNext())
-        {
-            String line = inFile.nextLine();
-            String[] words = line.trim().split(" ");
-            if(words[0].length()<=3) {
-            	int ukuran=Integer.parseInt(words[0]);
-            	learn.initUkuran(ukuran);
-            }else if(words[0].equalsIgnoreCase("Baru")) {
-            	String nama=words[1];
-               	int usia=Integer.parseInt(words[2]);
-               	int tensi=Integer.parseInt(words[3]);
-               	learn.baru(nama, usia, tensi);
-            }else if(words[0].equalsIgnoreCase("status")) {
-            	learn.status();
-            }else if(words[0].equalsIgnoreCase("selesai")) {
-            	if(words[1].length()<=2) {
-            		int index=Integer.parseInt(words[1]);
-			System.out.print("SELESAI_VAKSIN");	
-            		try{
-			   for (int i =0;i<index;i++){
-				learn.selesai(index);
-			   }
-            		}catch(Exception e) {
-            			System.err.println(e.getMessage());
-            		}
-            	}else if(words[1].length()>=3) {
-            		String nama=words[1];
-            		learn.selesai(nama);
-            	}
-            }else if(words[0].equalsIgnoreCase("ukuran")) {
-            	int ukuran=Integer.parseInt(words[1]);
-            	learn.ukuran(ukuran);
-            }else if(words[0].equalsIgnoreCase("skip")) {
-            	String nama=words[1];
-            	learn.skip(nama);
-            }
-        }
-        inFile.close();
+		FileReader fr = new FileReader("/C:\\Users\\alrav\\Dev\\Kuliah\\Semester-2\\Algorithhm Structure Data\\Practicise\\Tugas01\\tes.txt");
+		Scanner inFile = new Scanner(fr);
+		while (inFile.hasNext())
+		{
+			String line = inFile.nextLine();
+			String[] words = line.trim().split(" ");
+			if(words[0].length()<=3) {
+				int ukuran=Integer.parseInt(words[0]);
+				learn.initUkuran(ukuran);
+			}else if(words[0].equalsIgnoreCase("Baru")) {
+				String nama=words[1];
+				int usia=Integer.parseInt(words[2]);
+				int tensi=Integer.parseInt(words[3]);
+				learn.baru(nama, usia, tensi);
+			}else if(words[0].equalsIgnoreCase("status")) {
+				learn.status();
+			}else if(words[0].equalsIgnoreCase("selesai")) {
+				if(words[1].length()<=2) {
+					int index=Integer.parseInt(words[1]);
+					System.out.print("SELESAI_VAKSIN");
+					try{
+						for (int i =0;i<index;i++){
+							learn.selesai(index);
+						}
+					}catch(Exception e) {
+						System.err.println(e.getMessage());
+					}
+				}else if(words[1].length()>=3) {
+					String nama=words[1];
+					learn.selesai(nama);
+				}
+			}else if (words[0].equalsIgnoreCase("selesai_nama")){
+				learn.selesai(words[1]);
+		}else if(words[0].equalsIgnoreCase("ukuran")) {
+				int ukuran=Integer.parseInt(words[1]);
+				learn.ukuran(ukuran);
+			}else if(words[0].equalsIgnoreCase("skip")) {
+				String nama=words[1];
+				learn.skip(nama);
+			}
+		}
+		inFile.close();
 	}
 }
